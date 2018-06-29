53f3cc2db796b247432ce65edf1d74dc620356a6
Retterer <george.retterer@accenture.com>
2018-06-29 11:27:15 -0400

    Modified for API

--- src/main/java/LuhnCheck/LuhnController.java
+++ src/main/java/LuhnCheck/LuhnController.java
@@ -1,25 +1,25 @@
 package LuhnCheck;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/ccCheck/{value}")
 public class LuhnController {
 
     //This code runs, but the display is currently only a test screen - need to add js to make functional
-    @GetMapping("/ccCheck")
-    public void ccCheck(@RequestParam(value="ccNumber", required = false, defaultValue = "4111111111111111") String value, Model model) {
-        model.addAttribute("ccNumber", value);
+    @GetMapping
+    public String ccCheck(@PathVariable String value) {
+
         Long ccNumber = Long.parseLong(value);
 
         //add try block to and handle error
 
         LuhnChecker LuhnCheck = new LuhnChecker();
         String result = LuhnCheck.CardResult(ccNumber);
-        model.addAttribute("result", result);
+        return(result);
     }
 
 }
